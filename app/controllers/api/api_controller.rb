f262d091be421b2c50d9b20389ae03484c5045d3
Marty Trzpit <mtrizzy@gmail.com>
2014-01-07 16:19:32 -0600

    Allowing CORS for localhost

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -4,6 +4,8 @@ class Api::ApiController < ActionController::Base
   protect_from_forgery with: :null_session
 
   before_filter :authenticate_user_from_token!
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
 
   def authenticate_admin_user!
     redirect_to new_user_session_path unless current_user.try(:admin?)
@@ -36,6 +38,23 @@ class Api::ApiController < ActionController::Base
     #   unauthorized_token
     #   return
     # end
+  end
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == "OPTIONS"
+      headers['Access-Control-Allow-Origin'] = 'http://localhost'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+      headers['Access-Control-Max-Age'] = '1728000'
+      render text: '', content_type: 'text/plain'
+    end
   end
 
   def json_response_with_errors(errors, http_status)
