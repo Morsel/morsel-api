dac54994c8b4bfad8a599cbf735cbdad9a294fd3
Marty Trzpit <mtrizzy@gmail.com>
2014-02-11 23:08:03 -0600

    Updated API Docs with Response Objects to match JSON Serializers

--- app/serializers/user_with_private_attributes_serializer.rb
+++ app/serializers/user_with_private_attributes_serializer.rb
@@ -1,10 +1,10 @@
 class UserWithPrivateAttributesSerializer < UserSerializer
-  attributes :like_count,
+  attributes :draft_count,
+             :like_count,
              :morsel_count,
              :sign_in_count,
-             :twitter_username,
              :facebook_uid,
-             :draft_count
+             :twitter_username
 
   def like_count
     object.morsel_likes_for_my_morsels_by_others_count
