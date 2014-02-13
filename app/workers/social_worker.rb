996bb44eada6ab7206220f1035bfa18f96eea9b3
Marty Trzpit <mtrizzy@gmail.com>
2014-02-13 01:12:21 -0600

    Removed safe checks from social worker so that errors can be found

--- app/workers/social_worker.rb
+++ app/workers/social_worker.rb
@@ -3,15 +3,12 @@ class SocialWorker
 
   def perform(network, user_id, message)
     user = User.includes(:authorizations).find(user_id)
-    if user
-      case network
-      when :facebook
-        facebook_client = user.facebook_client
-        facebook_client.put_connections('me', 'feed', message: message) if facebook_client
-      when :twitter
-        twitter_client = user.twitter_client
-        twitter_client.update(message) if twitter_client
-      end
+    case network
+    when :facebook
+      user.facebook_client.put_connections('me', 'feed', message: message)
+    when :twitter
+      user.twitter_client.update(message)
+    end
     end
   end
 end
