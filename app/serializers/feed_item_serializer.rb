9d995945484403f322f48940f1ee61db4164eb97
Marty Trzpit <mtrizzy@gmail.com>
2014-05-16 18:02:12 -0500

    Updated API Docs

--- app/serializers/feed_item_serializer.rb
+++ app/serializers/feed_item_serializer.rb
@@ -2,7 +2,9 @@ class FeedItemSerializer < ActiveModel::Serializer
   attributes :id,
              :created_at,
              :updated_at,
-             :subject_type
+             :subject_type,
+             :featured,
+             :user_id
 
   has_one :subject
 end
