3245f228235686c21d46d1a458ffbd445aecf245
Marty Trzpit <mtrizzy@gmail.com>
2014-05-28 16:49:27 -0500

    Fixed issue w/ puma not loading due to deprecated Rails database_configuration

--- config/puma.rb
+++ config/puma.rb
@@ -8,7 +8,7 @@ workers Integer(ENV['PUMA_WORKER_COUNT'] || 3 )
 
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
-    config = Rails.application.config.database_configuration[Rails.env]
+    config = ActiveRecord::Base.configurations[Rails.env]
     config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
     config['pool']              = ENV['DB_POOL'] || 5
     ActiveRecord::Base.establish_connection(config)
