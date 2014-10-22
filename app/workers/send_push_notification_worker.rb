b5d9db91e1908b5d5162363dea4242507410261c
Marty Trzpit <mtrizzy@gmail.com>
2014-10-21 19:33:42 -0500

    No longer throwing an exception if a notification is deleted before it gets pushed [Fixes #81159470]

--- app/workers/send_push_notification_worker.rb
+++ app/workers/send_push_notification_worker.rb
@@ -5,7 +5,11 @@ class SendPushNotificationWorker
   def perform(options = nil)
     return if options.nil?
 
-    notification = Notification.find(options['notification_id'])
-    SendPushNotification.call(notification: notification) if notification
+    notification = Notification.find_by(id: options['notification_id'])
+    if notification
+      SendPushNotification.call(notification: notification)
+    else
+      Rollbar.report_message "Couldn't find Notification ##{options['notification_id']} push, ignoring.", 'info'
+    end
   end
 end
