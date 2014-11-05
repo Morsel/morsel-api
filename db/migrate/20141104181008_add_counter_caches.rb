4d94ca4ed8310e315b25b1b4b9415bb285a03e97
Marty Trzpit <mtrizzy@gmail.com>
2014-11-04 18:07:20 -0600

    changed `update` to `update_columns` in counter_cache migration

--- db/migrate/20141104181008_add_counter_caches.rb
+++ db/migrate/20141104181008_add_counter_caches.rb
@@ -27,35 +27,35 @@ class AddCounterCaches < ActiveRecord::Migration
   def update_item_counter_caches
     Item.reset_column_information
     Item.find_each do |i|
-      i.update comments_count: Comment.where(commentable_type:'Item', commentable_id:i.id).count
+      i.update_columns comments_count: Comment.where(commentable_type:'Item', commentable_id:i.id).count
     end
   end
 
   def update_keyword_counter_caches
     Keyword.reset_column_information
     Keyword.find_each do |k|
-      k.update followers_count: Follow.where(followable_id:k.id, followable_type:'Keyword').count
+      k.update_columns followers_count: Follow.where(followable_id:k.id, followable_type:'Keyword').count
     end
   end
 
   def update_morsel_counter_caches
     Morsel.reset_column_information
     Morsel.find_each do |m|
-      m.update likes_count: Like.where(likeable_type:'Morsel', likeable_id:m.id).count
+      m.update_columns likes_count: Like.where(likeable_type:'Morsel', likeable_id:m.id).count
     end
   end
 
   def update_place_counter_caches
     Place.reset_column_information
     Place.find_each do |p|
-      p.update followers_count: Follow.where(followable_id:p.id, followable_type:'Place').count
+      p.update_columns followers_count: Follow.where(followable_id:p.id, followable_type:'Place').count
     end
   end
 
   def update_user_counter_caches
     User.reset_column_information
     User.find_each do |u|
-      u.update  drafts_count: Morsel.drafts.where(creator_id: u.id).count,
+      u.update_columns  drafts_count: Morsel.drafts.where(creator_id: u.id).count,
                 followed_users_count: Follow.where(follower_id:u.id, followable_type:'User').count,
                 followers_count: Follow.where(followable_id:u.id, followable_type:'User').count
     end
