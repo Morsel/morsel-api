691d6252cc5ccbf8c4b978fdcb35ea393e934149
Marty Trzpit <mtrizzy@gmail.com>
2014-11-21 13:55:03 -0600

    Added convert all item likes to morsel likes script service.

---
+++ spec/services/convert_all_item_likes_to_morsel_likes_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe Scripts::ConvertAllItemLikesToMorselLikes do
+  subject(:service) { call_service }
+
+  let(:item_with_likes) { FactoryGirl.create(:item_with_likers, morsel: morsel, creator: morsel.creator, likes_count: likes_count) }
+  let(:morsel) { FactoryGirl.create(:morsel_with_creator) }
+  let(:likes_count) { 5 }
+
+  context 'morsels not liked yet' do
+    before do
+      item_with_likes
+      subject
+    end
+
+    it { should be_valid }
+    its(:response) { should eq([likes_count, 0]) }
+
+    it 'should like the morsels' do
+      expect(Like.where(likeable:morsel).count).to eq(likes_count)
+    end
+
+    it 'should destroy all existing Item likes' do
+      expect(Like.where(likeable:item_with_likes)).to be_empty
+    end
+  end
+
+  context 'morsel already liked' do
+    before do
+      item_with_likes
+      Like.create(liker:Like.first.liker, likeable: morsel)
+      subject
+    end
+
+    its(:response) { should eq([likes_count - 1, 1]) }
+
+    it 'should like the morsels' do
+      expect(Like.where(likeable:morsel).count).to eq(likes_count)
+    end
+
+    it 'should destroy all existing Item likes' do
+      expect(Like.where(likeable:item_with_likes)).to be_empty
+    end
+  end
+end
