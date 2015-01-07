67c231566270814243f843c6a79befb3232f3e7a
Marty Trzpit <mtrizzy@gmail.com>
2015-01-07 14:11:56 -0600

    Retry CollageWorker every 2 seconds while photos are still processing

--- app/workers/collage_worker.rb
+++ app/workers/collage_worker.rb
@@ -1,10 +1,20 @@
 class CollageWorker
   include Sidekiq::Worker
 
+  sidekiq_retry_in do |count|
+    2
+  end
+
   def perform(options = nil)
     return if options.nil?
 
     morsel = Morsel.find(options['morsel_id'])
+
+    if morsel.items.map(&:photo_processing).uniq.include?(true)
+      # photos still processing, retry job
+      raise Sidekiq::Retries::Retry.new(RuntimeError.new('item photos still processing'))
+    end
+
     service = GenerateCollage.call morsel: morsel
 
     morsel.update photo: service.response if service.valid?
